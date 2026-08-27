663cfedd424cc581d95751fc63abbe7f73b65097
Lucky44x <nickbali06@gmail.com>
2026-08-27 14:55:25 +0200

    Worked on further yaml demarshalling

--- commands/taste.go
+++ commands/taste.go
@@ -2,10 +2,20 @@ package commands
 
 import (
 	"context"
+	"fmt"
 
+	"github.com/lucky44x/barista/recipe"
 	"github.com/urfave/cli/v3"
 )
 
 func Taste(ctx context.Context, cmd *cli.Command) error {
+	recipeFile := cmd.StringArg("Recipe-file")
+	recipe, err := recipe.ReadRecipe(recipeFile)
+	if err != nil {
+		return err
+	}
+
+	fmt.Println(recipe)
+
 	return nil
 }
--- recipe/ingredient.go
+++ recipe/ingredient.go
@@ -1,12 +1,56 @@
 package recipe
 
-const TemplateIngredient = "template"
-const SymlinkIngredient = "symlink"
-const CopyIngredient = "copy"
-const CommandIngredient = "run"
+import (
+	"fmt"
+
+	"github.com/goccy/go-yaml"
+)
+
+type IngredientType string
+
+const (
+	IngredientTemplate IngredientType = "template"
+	IngredientSymlink  IngredientType = "symlink"
+	IngredientCopy     IngredientType = "copy"
+	IngredientRun      IngredientType = "run"
+)
+
+func (t IngredientType) Valid() bool {
+	switch t {
+	case IngredientTemplate,
+		IngredientSymlink,
+		IngredientCopy,
+		IngredientRun:
+		return true
+	default:
+		return false
+	}
+}
+
+func (t *IngredientType) UnmarshalYAML(data []byte) error {
+	var value string
+	if err := yaml.Unmarshal(data, &value); err != nil {
+		return err
+	}
+
+	parsed := IngredientType(value)
+	if !parsed.Valid() {
+		return fmt.Errorf(
+			"invalid ingredient of type %q; expected one of %q, %q, %q, %q",
+			value,
+			IngredientCopy,
+			IngredientRun,
+			IngredientSymlink,
+			IngredientTemplate,
+		)
+	}
+
+	*t = parsed
+	return nil
+}
 
 type Ingredient struct {
-	Type string `yaml:"type"`
+	Type IngredientType `yaml:"type"`
 
 	Source  string   `yaml:"source,omitempty"`
 	Target  string   `yaml:"target,omitempty"`
